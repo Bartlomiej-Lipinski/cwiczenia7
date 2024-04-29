edaaba12a14ee7b56ec3a1e83076880437264803
rochmisz <109878606+rochmisz@users.noreply.github.com>
2024-04-29 19:10:03 +0200

    WIP3

--- cwiczenia7/cwiczenia7/Endpoints/WareHouseEndPoint.cs
+++ cwiczenia7/cwiczenia7/Endpoints/WareHouseEndPoint.cs
@@ -7,6 +7,28 @@ public static class WareHouseEndPoint
 {
     public static void RegisterWarehouseEndpoints(this WebApplication app)
     {
-        
+        app.MapPost("/warehouse", (getOrder order,IDbService dbService) =>
+        {
+            var warehouse =order.WareHouseId;
+            var wh =dbService.GetWerehouse(warehouse);
+            if (wh is null)
+            {
+                Results.BadRequest("Warehouse not found");
+            }
+            var product = order.ProductId;
+            var p = dbService.GetProduct(product);
+            if (p is null)
+            {
+                Results.BadRequest("Product not found");
+            }
+
+            var o =dbService.GetOrder(order.ProductId);
+            if (o is null)
+            {
+                Results.BadRequest("Order Already exists");
+            }
+            
+            return Results.Ok($"Warehouse {warehouse} created");
+        });
     }
 }
